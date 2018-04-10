d5319c5f97ad1163b4959c9d376d407d30a91a45
TheCodeWw <327739288@qq.com>
2018-04-10 22:20:34 +0800

    Students

---
+++ Students.java
@@ -0,0 +1,73 @@
+package study17;
+
+public class Students implements Comparable<Students> {
+	private String name = null;
+	private int age = 0;
+
+	public Students() {
+
+	}
+
+	public Students(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Students other = (Students) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Students o) {
+		int tem = this.age - o.age; // tem计算出差值(此差值的正负会体现在自然排序中)
+		String name = this.name;
+		return tem == 0 ? name.compareTo(o.name) : tem; // tem越小越靠前
+	}
+
+}
